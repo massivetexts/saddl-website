285cab164f98e66696121945ca318de952e74586
Ben Schmidt <bmschmidt@gmail.com>
2021-07-01 13:58:03 -0400

    creating database

--- create_database.py
+++ create_database.py
@@ -13,17 +13,23 @@
 # ---
 
 import duckdb
+import sys
+
+try:
+    dbname, hfiles, LOCATION_OF_COMPARE_FILES = sys.argv[1:]
+except IndexError:
+    print("usage: python create_database.py dnname hathifile_parquet_location dir_with_compare_files")
+    exit(1)
 
 
-
-dathi = duckdb.connect("/drobo/hathi_db")
+dathi = duckdb.connect(dbname)
 
 # +
 # Creat hathifiles table--I don't have the code handy, but the important thing is that it's a parquet where every column is 
 # of string type, regardless of the underlying data.
 # -
 
-dathi.execute("CREATE TABLE hathifiles AS SELECT * FROM parquet_scan('/drobo/hathi_metafiles/20200201.parquet') ORDER BY htid")
+dathi.execute(f"CREATE TABLE hathifiles AS SELECT * FROM parquet_scan('{hfiles}') ORDER BY htid")
 
 dathi.execute("CREATE UNIQUE INDEX htid ON hathifiles (htid)")
 
@@ -49,9 +55,6 @@ for code in codes:
 schema = pa.schema(schema)
 output = parquet.ParquetWriter("out.parquet", schema)
 
-LOCATION_OF_COMPARE_FILES = "data"
-
-
 for i, p in enumerate(Path(LOCATION_OF_COMPARE_FILES).glob("**/*.parquet")):
     data = parquet.read_table(p)
     batch = {k : data[k] for k in codes}
@@ -62,7 +65,7 @@ for i, p in enumerate(Path(LOCATION_OF_COMPARE_FILES).glob("**/*.parquet")):
     print(i, end = "\r")
 # -
 
-dathi.execute("CREATE TABLE relationships AS SELECT * FROM parquet_scan('/drobo/saddl/example_predictions/out.parquet')")
+dathi.execute("CREATE TABLE relationships AS SELECT * FROM parquet_scan('out.parquet')")
 
 dathi.close()
 
