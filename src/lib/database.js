2135b02e333c59102b2d77005bef447211be1728
Peter Organisciak <organisciak@gmail.com>
2021-07-20 11:12:31 -0600

    Index building on db load

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,5 +1,15 @@
 
 import duckdb from 'duckdb';
-console.log("Regenerating database connection")
-const db = new duckdb.Database("saddl.duckdb")
+console.log("Regenerating database connection");
+const db = new duckdb.Database("saddl.duckdb");
+db.all('CREATE INDEX htid_idx ON meta (htid);', function(err, res) {
+    if (err) {
+      throw err;
+    }
+  });
+db.all('CREATE INDEX target_idx ON clean_predictions (target);', function(err, res) {
+    if (err) {
+        throw err;
+    }
+});
 export const con = db.connect();
