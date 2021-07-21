659b3ecb611af283183e1c067bca3ba3de086b0e
Peter Organisciak <organisciak@gmail.com>
2021-07-20 20:43:57 -0600

    Initial styling

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,119 +1,72 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+  import { page } from "$app/stores";
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+  <div class="corner">
+    <span class="bighead">SaDDL</span><span class="subhead">Similarities and Duplicates in Digital Libraries</span>
+  </div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.path === '/catalog'}><a sveltekit:prefetch href="/catalog">Catalog</a></li>
-			<li class:active={$page.path === '/relationships'}><a sveltekit:prefetch href="/relationships">Relationships</a></li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li class="navbar-item" class:active={$page.path === "/catalog"}>
+        <a class="navbar-link" sveltekit:prefetch href="/catalog">Catalog</a>
+      </li>
+      <li class="navbar-item" class:active={$page.path === "/relationships"}>
+        <a class="navbar-link" sveltekit:prefetch href="/relationships">Relationships</a>
+      </li>
+    </ul>
+  </nav>
 
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
+  <div class="corner">
+    <!-- TODO put something else here? github link? -->
+  </div>
 </header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 10%;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
-	}
+  .corner {
+    letter-spacing: 0.3rem;
+  }
+  .subhead {
+    margin-left: 10px;
+    color: #777;
+    font-size: 15px;
+  }
+  .bighead {
+    font-size: 20px;
+    text-shadow: -1px -1px;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+  .navbar {
+    display: block;
+    width: 100%;
+    height: 3.5rem;
+    background: #fff;
+    z-index: 99;
+    border-top: 1px solid #eee;
+    border-bottom: 1px solid #eee;
+  }
+  .navbar-list {
+    list-style: none;
+    margin-bottom: 0;
+  }
+  .navbar-item {
+    position: relative;
+    float: left;
+    margin-bottom: 0;
+  }
+  .navbar-link {
+    /*text-transform: uppercase;*/
+    font-size: 11px;
+    font-weight: 600;
+    letter-spacing: 0.2rem;
+    margin-right: 35px;
+    text-decoration: none;
+    line-height: 3.5rem;
+    color: #222;
+  }
+  .navbar-link:hover {
+    text-shadow: 1px 1px grey;
+  }
 </style>
--- src/routes/relationships/index.svelte
+++ src/routes/relationships/index.svelte
@@ -2,32 +2,38 @@
 </script>
 
 <script>
-import { onMount } from "svelte";
-import { Circle2 } from 'svelte-loading-spinners';
-const encode = (str) => encodeURIComponent(str.replaceAll("/", "===="))
+  import { onMount } from "svelte";
+  import { Circle2 } from "svelte-loading-spinners";
+  const encode = (str) => encodeURIComponent(str.replaceAll("/", "===="));
 
-
-let my_ids = undefined;
-onMount( () => {
-  const url = "/relationships/random.json";
-  my_ids = fetch(url)
-  .then(
-    response => response.json()
-  );
-})
+  let my_ids = undefined;
+  onMount(() => {
+    const url = "/relationships/random.json";
+    my_ids = fetch(url).then((response) => response.json());
+  });
 </script>
 
 <h1>Random Relationships</h1>
 
 For now, I'm just fetching a bunch of random HTIDs.
 {#if my_ids}
-{#await my_ids}
-	<Circle2></Circle2>
-{:then id_list}
-	{#each id_list as id}
-    <a href="/relationships/{encode(id.htid)}/">    
-      {id.title} - {id.author}
-    </a>
-    {/each}
-{/await}
+  {#await my_ids}
+    <Circle2 />
+  {:then id_list}
+    <div class="row">
+      {#each id_list as id}
+        <div class="three columns">
+          <p>
+            <strong>
+              <a href="/relationships/{encode(id.htid)}/"> {id.title} ({id.rights_date_used})</a>
+            </strong>
+          </p>
+          <ul>
+            <li>{id.description}</li>
+            <li>{id.author}</li>
+          </ul>
+        </div>
+      {/each}
+    </div>
+  {/await}
 {/if}
