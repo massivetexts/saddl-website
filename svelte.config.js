71105c90149aba8f69b059b4c2cdfef6ba971e5d
Peter Organisciak <organisciak@gmail.com>
2021-07-20 20:40:24 -0600

    Add favicon and remove boilerplate assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
-import node from '@sveltejs/adapter-node';
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: node(),
-		target: '#svelte',
-		vite: {
-			define: {
-//				'process.env': {}
-			}
-		}
-	},
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: node(),
+    target: "#svelte",
+    vite: {
+      define: {
+        //				'process.env': {}
+      },
+    },
+  },
 };
 
 export default config;
