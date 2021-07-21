25156b7807afac05e180ab5f22f06401d785c4bb
Peter Organisciak <organisciak@gmail.com>
2021-07-21 17:34:59 -0600

    Row tiling solution

--- src/routes/relationships/index.svelte
+++ src/routes/relationships/index.svelte
@@ -24,10 +24,16 @@ For now, I'm just fetching a bunch of random HTIDs.
   {#await my_ids}
     <Circle2 />
   {:then id_list}
-    <div class="row">
-      {#each id_list as id, i}
-        <VolumeCard meta={id} />
-      {/each}
-    </div>
+    {#each { length: Math.ceil(id_list.length / 3) } as _, i}
+      <div class="row">
+        <VolumeCard meta={id_list[i * 3]} />
+        {#if i * 3 + 1 < id_list.length}
+          <VolumeCard meta={id_list[i * 3 + 1]} />
+        {/if}
+        {#if i * 3 + 2 < id_list.length}
+          <VolumeCard meta={id_list[i * 3 + 2]} />
+        {/if}
+      </div>
+    {/each}
   {/await}
 {/if}
